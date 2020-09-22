d195ded4181bd2b8e9ecb903056e6fe57d0ec4ab
dpetzoldt <Dirk.Petzoldt@gmail.com>
2020-09-22 03:22:25 +0200

    clean old backlinks

--- zettel/backlinks.py
+++ zettel/backlinks.py
@@ -1,4 +1,4 @@
-from zettel.util import api, notes_tagged
+from util import api, notes_tagged
 from asyncio import run, wait
 from dataclasses import dataclass
 from httpx import Response
@@ -11,14 +11,14 @@ import re
 backlinks_regex = re.compile('\n\^:link:\^.*')
 
 def write_backlinks_markdown(notelink_list):
-    return f"\n^:link:^{', '.join([l.markdown() for l in notelink_list])}\n"
+    return f"\n\n^:link:^{', '.join([l.markdown() for l in notelink_list])}\n"
 
 def remove_backlinks_markdown(markdown_string):
     match = backlinks_regex.search(markdown_string)
     if match:
         start = match.regs[0][0]
         end = match.regs[0][1]
-        return some_markdown[: start] + some_markdown[end : ]
+        return markdown_string[: start] + markdown_string[end : ]
     else:
         return markdown_string 
 
@@ -30,7 +30,6 @@ class NoteLink:
     def markdown(self) -> str:
         return f"[{self.text}](:/{self.id})"
 
-INTRO = "\n\n* * *\nBacklinks\n"
 
 async def note_data(note):
     if isinstance(note, str):
@@ -44,14 +43,37 @@ async def add_backlinks(note):
     """Append backlinks to note and update at server """
 
     note = await note_data(note)
-    logging.info(f"Adding backlinks: {note['id']}")
-    linking_notes = (await api().search(note['id'])).json()
-    links = [NoteLink(n['id'],n['title']) for n in linking_notes if n['id'] != note['id']]
+    nid = note.pop('id')
+    body = note.pop('body')
+    logging.info(f"Adding backlinks: {nid}")
+
+    # Temporary code to clean up deprecated backlinks
+    match = re.search("\n---\nBacklinks\n", body)
+    if match:
+        start = match.regs[0][0]
+        body = body[:start]
+
     # remove old backlinks (if any)
-    body = remove_backlinks_markdown(note['body'])
-    # add backlinks and upload
-    body += write_backlinks_markdown(links)
-    await api().update_note(note['id'],note['title'],body, note['parent_id'])
+        body = remove_backlinks_markdown(body)
+
+    # Find in-links from all other notes. Keep those not already contained
+    linking_notes = (await api().search(nid)).json()
+    links = [NoteLink(n['id'],n['title']) for n in linking_notes \
+        if (n['id'] != nid) and (n['id'] not in body)]
+    
+    if links:
+
+        # add backlinks and upload
+        body += write_backlinks_markdown(links)[:-1] # drop closing newline
+
+        # fields not passed (again) into `update_note` are lost
+        title = note.pop('title')
+        pid = note.pop('parent_id')
+        tags = (await api().get_notes_tags(nid)).json()
+        # tags were not returned by `get_note` but required during update
+        note['tags'] = ', '.join([t['title'] for t in tags])
+        # upload changed body and confirm all other fields
+        await api().update_note(nid, title, body, pid, **note)
 
 
 async def main(tag):
