455df83258eac9e88faa0441a2ebba53b21c1214
dpetzoldt <Dirk.Petzoldt@gmail.com>
2021-06-09 08:58:08 +0200

    cleaning up

--- core/cli.py
+++ core/cli.py
@@ -11,18 +11,7 @@ sys.path.insert(0, '/Users/dpetzoldt/git/home/joplin-api')
 import server, markdown, scripts, backlinks
 
     
-async def launch(args):
-    """
-        if cmd:
-            if cmd['robot'] == 'tag_report':
-                await tag_report(cmd['label'])
-                with open(log_file) as file:  
-                    note['body'] = note['body'] + '\n* * *\n' + file.read()
-                    await server.update_note(note)
-            else:
-                logging.warning(f"Unknown script: {cmd['robot']}")
-    """
-
+async def launch_command(args):
     if args.cmd == 'scripts':
         result = await scripts.find()
         logging.info(result)
@@ -32,34 +21,34 @@ async def launch(args):
         notes = await server.search(args.search)
         for n in notes:
             print(n)
+        print(len(notes))   
 
 def config_log(debug, log_file = 'app.log'):
     logging.basicConfig(
         level=logging.DEBUG if debug else logging.INFO,
-        format='%(levelname)s:%(message)s', 
-        handlers=[logging.FileHandler(log_file,'w'), logging.StreamHandler()]
+        format='%(levelname)s:%(message)s' 
+       #,handlers=[logging.FileHandler(log_file)]#, logging.StreamHandler()]
         )
     logging.debug(f"Zelda launching at {datetime.now()}.")
 
-def config_arg_parser():
-    return None
-
-def run():
+def config_args():
     parser = argparse.ArgumentParser(description='Turns Joplin into a Zettelkasten')
     parser.add_argument('cmd', help=f"The command Zelda will run", \
         choices=['scripts', 'backlinks', 'notes'])
     parser.add_argument('par', nargs='*', help=f"Command parameters (optional)")
-    parser.add_argument("-d", "--debug", help="Print debug messages", action="store_true")
+    parser.add_argument("-d", "--debug", help="Print debug messages", action='store_true')
     parser.add_argument("-t", "--tag", help="Only process notes with this tag")
     parser.add_argument("-s", "--search", help="Only processes notes that match the search")
-    
-    args = parser.parse_args(sys.argv[1:])
+    return parser
+
+def run_application():
+    args = config_args().parse_args(sys.argv[1:])
     config_log(args.debug)
-
-    asyncio.run(launch(args))
-
+    #asyncio.run(launch_command(args))
+    # file_handler.close()
 
 if __name__ == '__main__':
-    # debug:
-    sys.argv = [sys.argv[0]] + 'notes -s data'.split()
-    run()
+    # if no parameters debug with following parameters
+    if len(sys.argv) == 1:
+        sys.argv = [sys.argv[0]] + 'notes -d -s data'.split()
+    run_application()
