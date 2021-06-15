a16009ddd2fb6179827dd84350e59d1743d07c7d
dpetzoldt <Dirk.Petzoldt@gmail.com>
2021-06-15 19:57:15 +0200

    client updates

--- core/cli.py
+++ core/cli.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # see https://realpython.com/run-python-scripts/#using-the-script-filename
 
-""" Entry point for application from 
-"""
+""" Application accessible as Command Line Interface """
 
 import logging, asyncio, sys
 from datetime import datetime
-import argparse # see https://docs.python.org/3/library/argparse.html#module-argparse
+from argparse import ArgumentParser # see https://docs.python.org/3/library/argparse.html#module-argparse
 sys.path.insert(0, '/Users/dpetzoldt/git/home/joplin-api')
 import server, markdown, scripts, backlinks
 
@@ -23,32 +22,35 @@ async def launch_command(args):
             print(n)
         print(len(notes))   
 
-def config_log(debug, log_file = 'app.log'):
+def config_log(debug_flag, log_file = '.log'):
     logging.basicConfig(
-        level=logging.DEBUG if debug else logging.INFO,
+        level=logging.DEBUG if debug_flag else logging.INFO,
         format='%(levelname)s:%(message)s' 
-       #,handlers=[logging.FileHandler(log_file)]#, logging.StreamHandler()]
+        ,handler = logging.FileHandler(log_file)#, logging.StreamHandler()]
         )
     logging.debug(f"Zelda launching at {datetime.now()}.")
 
 def config_args():
-    parser = argparse.ArgumentParser(description='Turns Joplin into a Zettelkasten')
-    parser.add_argument('cmd', help=f"The command Zelda will run", \
-        choices=['scripts', 'backlinks', 'notes'])
-    parser.add_argument('par', nargs='*', help=f"Command parameters (optional)")
-    parser.add_argument("-d", "--debug", help="Print debug messages", action='store_true')
-    parser.add_argument("-t", "--tag", help="Only process notes with this tag")
-    parser.add_argument("-s", "--search", help="Only processes notes that match the search")
-    return parser
+    p = ArgumentParser(description='Turns Joplin into a Zettelkasten')
+    add = p.add_argument
+    add(
+        'cmd', help=f"The command Zelda will run", \
+        choices=['scripts', 'backlinks', 'notes'])    
+    add('par', nargs='*', help=f"Command parameters (optional)")
+    add("-d", "--debug",  help="Print debug messages", action='store_true')
+    add("-t", "--tag",    help="Only process notes with this tag")
+    add("-s", "--search", help="Only processes notes that match the search")
+    return p
 
-def run_application():
+def run_client():
+    """ Entry point when starting the application. """
     args = config_args().parse_args(sys.argv[1:])
     config_log(args.debug)
     #asyncio.run(launch_command(args))
     # file_handler.close()
 
 if __name__ == '__main__':
-    # if no parameters debug with following parameters
-    if len(sys.argv) == 1:
-        sys.argv = [sys.argv[0]] + 'notes -d -s data'.split()
-    run_application()
+    # if no parameters (except path to script) debug with some hardcoded parameters
+    if len(sys.argv) <= 1:
+        sys.argv += 'notes -d -s data'.split()
+    run_client()
